00282535edc8e24898a4b1f3c4b8305b092249ec
FaridRifaan <faridrifan59@gmail.com>
2023-03-27 15:34:02 +0700

    initial commit

--- app/src/main/java/com/latihan_viewpager/MainActivity.kt
+++ app/src/main/java/com/latihan_viewpager/MainActivity.kt
@@ -2,10 +2,29 @@ package com.latihan_viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var viewPager = findViewById<ViewPager>(R.id.view_pager)
+        var tabs = findViewById<TabLayout>(R.id.tab_layout_view)
+
+        val adapter = AdapterFragment(supportFragmentManager)
+        adapter.addFragment(HomeFragment(), "Home")
+        adapter.addFragment(FavoriteFragment(), "Favorit")
+        adapter.addFragment(SettingFragment(), "Setting")
+
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        tabs.getTabAt(0)!!.setIcon(R.drawable.baseline_home_24)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.baseline_favorite_24)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.baseline_settings_24)
     }
+
+
 }
